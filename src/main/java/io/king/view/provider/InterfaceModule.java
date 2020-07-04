34d513c942e851340e36244563c3cc6c71e9e204
zkingboos <kingboosoficial@gmail.com>
2020-07-04 16:36:32 -0300

    updated things

--- src/main/java/io/king/view/provider/InterfaceModule.java
+++ src/main/java/io/king/view/provider/InterfaceModule.java
@@ -2,12 +2,17 @@ package io.king.view.provider;
 
 import io.king.core.api.cycle.LifeContext;
 import io.king.core.api.cycle.LifeCycle;
+import io.king.core.api.module.ModuleConfig;
+import io.king.core.provider.cycle.event.ModuleInitialized;
 import io.king.core.api.module.Module;
 import io.king.core.api.module.ModulePriority;
+import io.king.core.provider.module.ModuleObject;
 import io.king.view.provider.command.ModuleView;
 import io.king.view.provider.time.TimeService;
 import io.king.view.provider.view.EntityGui;
 import io.king.view.provider.view.ViewService;
+
+import java.util.logging.Logger;
 
 @Module(
         services = {
@@ -21,7 +26,16 @@ public final class InterfaceModule extends LifeCycle {
 
     @Override
     public void init(LifeContext context) {
-        context.getLogger().info("Interface module loaded");
+        final Logger logger = context.getLogger();
+        context.registerEvent(ModuleInitialized.class, instance -> {
+            final ModuleConfig moduleConfig = instance.getModuleObject().getModuleConfig();
+
+            final String logMessage = String.format(
+                    "Module %s has been loaded.",
+                    moduleConfig.getName()
+            );
+            logger.info(logMessage);
+        });
     }
 
     @Override
